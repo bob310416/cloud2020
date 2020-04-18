ab4fac3aa1bbc0079072b2d332836dd05f8e7c61
huangxb <huangxb@ctrip.com>
2020-04-18 21:35:54 +0800

    增加一个module:boot-launch来写springboot2 相关的demo

---
+++ boot-launch/src/main/java/com/bootest/BootLaunchApplication.java
@@ -0,0 +1,16 @@
+package com.bootest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: bob
+ * Date: 2020/4/18 - 21:10
+ */
+@SpringBootApplication
+public class BootLaunchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootLaunchApplication.class);
+
+    }
+}
